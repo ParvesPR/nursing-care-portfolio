a9297ae45d4ec2301a543a125fc5b41733e52b62
ParvesPR <parves2585@gmail.com>
2024-01-18 23:19:17 +0800

    responsive navbar

---
+++ public/script.js
@@ -0,0 +1,5 @@
+let toggle_btn = document.getElementById("toggle-btn");
+let navbar = document.getElementById("navbar");
+toggle_btn.addEventListener("click", () => {
+    navbar.classList.toggle("hidden")
+})
